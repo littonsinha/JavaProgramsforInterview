41f52e1592e5000e78db07410d5321e3277321a6
Litton <18619601+littonsinha@users.noreply.github.com>
2020-01-09 16:35:34 +0530

    add junk program

---
+++ src/interviewPractice/RemoveJunkCharacters.java
@@ -0,0 +1,14 @@
+package interviewPractice;
+
+public class RemoveJunkCharacters
+{
+	public static void main(String[] args)
+	{
+		String a = "!@#$Litton)(&((()";
+		a = a.replaceAll("[^A-Za-z0-9]", "");
+		System.out.println(a);
+		
+		
+	}
+
+}
