d1600b135b5e0f5322eddb76ed784523d35b027e
Litton <18619601+littonsinha@users.noreply.github.com>
2020-01-22 19:33:14 +0530

    push Strings

--- src/interviewPractice/StringMethods.java
+++ src/interviewPractice/StringMethods.java
@@ -1,11 +1,56 @@
 package interviewPractice;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.ListIterator;
+
 public class StringMethods
 {
 	public static void main(String [] args)
 	{
+		ArrayList<String> a = new ArrayList<String>();
+		a.add("Sony");
+		a.add("Star");
+		a.add("Zee");
+		a.add("Colors");
+		System.out.println("--------for Each with Lambda Exp-----------");		
+		a.forEach(tv ->	
+		{
+			System.out.println(tv);
+		});
+		System.out.println("--------------Iterator-----------");
 		
-	}
-	
-	
+		Iterator<String> itr = a.iterator();
+		
+		while (itr.hasNext())
+		{
+			String tv = itr.next();
+			System.out.println(tv);
+		
+		}
+		System.out.println("--------Iterator with Lambda Exp----------");
+		
+		itr = a.iterator();
+		itr.forEachRemaining(tv ->
+		{
+			System.out.println(tv);
+		});
+		
+		System.out.println("--------For each loop-Enhanced For loop---------------------");
+		
+		for(String tv: a)
+		{
+			System.out.println(tv);
+		}	
+		
+		System.out.println("--------List Iterator---------------------");
+		
+		ListIterator<String> tv = a.listIterator(a.size());
+		
+		while(tv.hasPrevious())
+		{
+			String previous = tv.previous();
+			System.out.println(previous);
+		}	
+	}	
 }
