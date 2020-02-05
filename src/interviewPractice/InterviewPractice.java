44ee1ef13f7f4b9607ea8b3f4facca54db946719
Litton <18619601+littonsinha@users.noreply.github.com>
2020-02-05 14:46:02 +0530

    practice

--- src/interviewPractice/InterviewPractice.java
+++ src/interviewPractice/InterviewPractice.java
@@ -1,13 +1,28 @@
 package interviewPractice;
 
 public class InterviewPractice 
-{	
-	public static void main(String args[])
+{
+	public static void isArmstrong(int num)
 	{
+		int cube=0;
+		int reverse;
+		int temp;
+		temp=num;
 		
-		
-		
+		while(num>0)
+		{
+			reverse=num%10;
+			num=num/10;
+			cube=cube+(reverse*reverse*reverse);
+		}
+			if(temp==cube)
+				System.out.println("Armstrong");
+			else
+				System.out.println("Not Armstrong");
 	}
 	
-
+	public static void main(String args[])
+	{
+		isArmstrong(153);;	
+	}
 }
